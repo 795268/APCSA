7518e86fe272ed5392d3ca25e333896cd0ce5f3e
795268 <42563195+795268@users.noreply.github.com>
2018-12-06 10:33:12 -0800

    working on A9

---
+++ projects/Project 5 - Picture Lab/pixLab/classes/classes/PictureTester.java
@@ -0,0 +1,197 @@
+/**
+ * This class contains class (static) methods
+ * that will help you test the Picture class 
+ * methods.  Uncomment the methods and the code
+ * in the main to test.
+ * 
+ * @author Barbara Ericson 
+ */
+public class PictureTester
+{
+    /** Method to test zeroBlue */
+    public static void testZeroBlue()
+    {
+        Picture beach = new Picture("HOTdog.jpg");
+        beach.explore();
+        beach.zeroBlue();
+        beach.explore();
+    }
+
+    /**Method to test keepOnlyBlue */
+
+    public static void onlyBlue()
+    {
+        Picture beach = new Picture("beach.jpg");
+        beach.explore();
+        beach.onlyBlue();
+        beach.explore();
+    }
+
+    /** Method to test mirrorVertical */
+    public static void testMirrorVertical()
+    {
+        Picture caterpillar = new Picture("caterpillar.jpg");
+        caterpillar.explore();
+        caterpillar.mirrorVertical();
+        caterpillar.explore();
+    }
+
+    /** Method to test mirrorVerticalRightToLeft */
+    public static void testMirrorVerticalRightToLeft()
+    {
+        Picture caterpillar = new Picture("caterpillar.jpg");
+        caterpillar.explore();
+        caterpillar.mirrorVerticalRightToLeft();
+        caterpillar.explore();
+    }
+
+    /** Method to test mirrorHorizontal */
+    public static void testMirrorHorizontal()
+    {
+        Picture caterpillar = new Picture("caterpillar.jpg");
+        caterpillar.explore();
+        caterpillar.mirrorHorizontal();
+        caterpillar.explore();
+    }
+
+    /** Method to test mirrorHorizontalBotToTop */
+    public static void testMirrorHorizontalBotToTop()
+    {
+        Picture caterpillar = new Picture("caterpillar.jpg");
+        caterpillar.explore();
+        caterpillar.mirrorHorizontalBotToTop();
+        caterpillar.explore();
+    }
+
+
+    /** Method to test mirrorTemple */
+    public static void testMirrorTemple()
+    {
+        Picture temple = new Picture("temple.jpg");
+        temple.explore();
+        temple.mirrorTemple();
+        temple.explore();
+
+    }
+
+    /** Method to test mirrorArms */
+    public static void testMirrorArms()
+    {
+        Picture temple = new Picture("snowman.jpg");
+        temple.explore();
+        temple.mirrorArms();
+        temple.explore();
+
+    }
+
+    /** Method to test mirrorGull */
+    public static void testMirrorGull()
+    {
+        Picture temple = new Picture("seagull.jpg");
+        temple.explore();
+        temple.mirrorGull();
+        temple.explore();
+
+    }
+
+    /** method to test copy*/ 
+
+    public static void testCopy()
+    {
+        Picture canvas = new Picture("640x480.jpg");
+        canvas.createCollage();
+        canvas.explore();
+    }
+
+    /** Method to test the collage method */
+    public static void testCollage()
+    {
+        Picture canvas = new Picture("640x480.jpg");
+        canvas.createCollage();
+        canvas.explore();
+    }
+    
+    /** Method to test the collage method */
+    public static void testmyCollage()
+    {
+        Picture canvas = new Picture("640x480.jpg");
+        canvas.myCollage();
+        canvas.explore();
+    }
+
+    /** Method to test edgeDetection */
+    public static void testEdgeDetection()
+    {
+        Picture swan = new Picture("swan.jpg");
+        swan.edgeDetection(10);
+        swan.explore();
+    }
+    
+     /** Method to test myedgeDetection */
+    public static void testmyEdgeDetection()
+    {
+        Picture me = new Picture("hotdog.jpg");
+        me.edgeDetection(10);
+        me.explore();
+    }
+    
+     public static void edgeColor()
+    {
+        Picture edge = new Picture("hotdog.jpg");
+        edge.colorEdge(10);
+        edge.explore();
+    }
+    
+
+    /**Method to test Negate*/
+    public static void Negate(){
+        Picture beach = new Picture("beach.jpg");
+        beach.Negate();
+        beach.explore();
+    }
+
+    /** Method to test grayscale*/
+    public static void Grayscale() {
+        Picture beach = new Picture("beach.jpg");
+        beach.Grayscale(); 
+        beach.explore(); 
+
+    }
+
+    public static void fixUnderwater() { 
+        Picture beach = new Picture("water.jpg"); 
+        beach.fixUnderwater(); 
+        beach.explore(); 
+    }
+
+    /** Main method for testing.  Every class can have a main
+     * method in Java */
+    public static void main(String[] args)
+    {
+        // uncomment a call here to run a test
+        // and comment out the ones you don't want
+        // to run
+        //testZeroBlue();
+        onlyBlue();
+        //testKeepOnlyRed();
+        //testKeepOnlyGreen();
+        //testNegate();
+        //testGrayscale();
+        //testFixUnderwater();
+        //testMirrorVertical();
+        //testMirrorTemple();
+        //testMirrorArms();
+        //testMirrorGull();
+        //testMirrorDiagonal();
+        //testCollage();
+        //testCopy();
+        //testEdgeDetection();
+        //testEdgeDetection2();
+        //testChromakey();
+        //testEncodeAndDecode();
+        //testGetCountRedOverValue(250);
+        //testSetRedToHalfValueInTopHalf();
+        //testClearBlueOverValue(200);
+        //testGetAverageForColumn(0);
+    }
+}
