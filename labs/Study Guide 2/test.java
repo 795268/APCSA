1c9ccefce8a1adb85811ac7c0aaf22070010c6c2
795268 <42563195+795268@users.noreply.github.com>
2018-12-15 11:55:59 -0800

    study

---
+++ labs/Study Guide 2/test.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class test here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class test
+{
+    public static void one() {
+        int m = 0;
+        int n = 7;
+        while(m < 3) {
+            n--;
+            m++;
+        }
+        System.out.print("" + m + n);
+
+    }
+}
